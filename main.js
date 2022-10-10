0b85580a93459d2d1506a9e861fecff8615b652e
Przemysław Botwina <przemek.botwina@o2.pl>
2022-10-10 18:47:49 +0200

    add html and css structure

---
+++ main.js
@@ -0,0 +1,12 @@
+class Main {
+  constructor() {
+    this.div = document.querySelector('.div-list');
+    this.addBtn = document.querySelector('.add-btn')
+    this.searchBtn = document.querySelector('.serach-btn')
+  }
+}
+
+const main = new Main();
+//1 klasa addRmv
+//2 klasa edit
+//3 klasa search
